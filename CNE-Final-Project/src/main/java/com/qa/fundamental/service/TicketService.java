aa2e13cbe09fccb557c71caeab5b5536e2ca33b7
tayo-paseda <tayopaseda@gmail.com>
2021-04-26 12:01:41 +0100

    added create ticket method to service

--- CNE-Final-Project/src/main/java/com/qa/fundamental/service/TicketService.java
+++ CNE-Final-Project/src/main/java/com/qa/fundamental/service/TicketService.java
@@ -3,6 +3,7 @@ package com.qa.fundamental.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.qa.fundamental.persistence.domain.Ticket;
 import com.qa.fundamental.persistence.repository.TicketRepo;
 
 @Service
@@ -16,7 +17,9 @@ public class TicketService {
 		this.repo = repo;
 	}
 	
-	
+	public Ticket create(Ticket ticket) {
+		return this.repo.save(ticket);
+	}
 	
 	
 
