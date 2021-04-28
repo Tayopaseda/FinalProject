b8f5a9f271276767adf545ef5e1750e123616d2c
aadil-moghal <aadilmoghal@gmail.com>
2021-04-28 11:32:25 +0100

    cleaned up

--- frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.jsx
+++ frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 import React from "react";
-import Avatar from "react-avatar";
 import { Col, Row, Button, Card, Accordion } from "react-bootstrap";
-import * as Icon from "react-bootstrap-icons";
 const DeleteTicket = ({
   id,
   title,
