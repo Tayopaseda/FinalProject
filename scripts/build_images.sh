3b208a5638bff0e210247539e3b4fab2069214bd
tayo-paseda <tayopaseda@gmail.com>
2021-04-29 12:45:48 +0100

    added frontend docker images

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -4,23 +4,22 @@ echo "building test docker images"
 
 echo "building backend image"
 cd ./CNE-Final-Project
-sudo docker build -t backend:test .
+sudo docker build -t backend:latest .
 
 cd ../frontend/help_queue_p3
-sudo docker build -t frontend:test .
+sudo docker build -t frontend:latest .
 
 #push backend images to docker hub
 echo "pushing backend images to dockerhub"
 echo $dockerPassword | sudo docker login --username=$dockerUsername --password-stdin
-sudo docker tag backend:test tayop/backend:test
-sudo docker push tayop/backend:test
+sudo docker tag backend:latest tayop/backend:latest
+sudo docker push tayop/backend:latest
 
 #push frontend images to docker hub
 echo "pushing frotnend images to dockerhub"
-sudo docker tag frontend:test tayop/frontend:test
-sudo docker push tayop/frontend:test
+sudo docker tag frontend:latest tayop/frontend:latest
+sudo docker push tayop/frontend:latest
 
 echo "deleting images from jenkins system"
-sudo docker rmi frontend:test tayop/frontend:test backend:test tayop/backend:test
+sudo docker rmi frontend:latest tayop/frontend:latest backend:latest tayop/backend:latest
 sudo docker system prune -a -f
-
