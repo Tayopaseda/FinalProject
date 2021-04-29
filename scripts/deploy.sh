f689d24ed97811d4c974a26c59d0a3d45637722c
tayo-paseda <tayopaseda@gmail.com>
2021-04-29 12:37:47 +0100

    added deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+cd ./kubernetes
+
+kubectl apply -f namespace.yaml
+kubectl config set-context --current --namespace=production
+kubectl apply -f nginx-config.yaml 
+kubectl apply -f nginx-lb.yaml -f nginx.yaml -f backend.yaml -f backend-dep.yaml -f frontend.yaml -f frontend-dep.yaml 
+
+kubectl rollout restart deployment backend
+kubectl rollout restart deployment help-queue
+
+sleep 3
+
+kubectl get services nginx-lb
