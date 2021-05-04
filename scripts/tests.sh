08fd52afd0c9b25ecf076191b21497318078662b
Tayo <t.paseda@warwick.ac.uk>
2021-05-04 11:30:46 +0100

    Update tests.sh

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -5,8 +5,8 @@ source ~/.bash_profile
 
 #substitute variables for values from env variables
 sed -i "s/test-vm/$testIP/g" ./ansible/inventory.yaml 
-sed -i "s/{{password}}/$dockerPassword/g" ./ansible/run-tests/tasks/main.yml
-sed -i "s/{{username}}/$dockerUsername/g" ./ansible/run-tests/tasks/main.yml
+sed -i "s/{{password}}/$dockerPassword/g" ./ansible/push-images/tasks/main.yml
+sed -i "s/{{username}}/$dockerUsername/g" ./ansible/push-images/tasks/main.yml
 
 #run ansible playbook
 cd ./ansible
@@ -16,5 +16,5 @@ cd ..
 
 #substitute variables in ansible directory back
 sed -i "s/$testIP/test-vm/g" ./ansible/inventory.yaml 
-sed -i "s/$dockerPassword/{{password}}/g" ./ansible/run-tests/tasks/main.yml
-sed -i "s/$dockerUsername/{{username}}/g" ./ansible/run-tests/tasks/main.yml
+sed -i "s/$dockerPassword/{{password}}/g" ./ansible/push-images/tasks/main.yml
+sed -i "s/$dockerUsername/{{username}}/g" ./ansible/push-images/tasks/main.yml
