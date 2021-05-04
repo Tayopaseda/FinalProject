35ea6c4688e3e12f677ada9ef3ea861c22749b2b
Tayo <t.paseda@warwick.ac.uk>
2021-05-04 12:00:57 +0100

    Update ViewTickets.jsx

--- frontend/help_queue_p3/src/Components/TicketComponent/ViewTickets.jsx
+++ frontend/help_queue_p3/src/Components/TicketComponent/ViewTickets.jsx
@@ -30,7 +30,7 @@ const completeTicket = (e) => {
 
 
     axios
-    .put("http://a3129e30ad6a545b0bee2a8723b3a26e-2018857003.eu-west-2.elb.amazonaws.com:8081/ticket/completed/" + id, ticketValues, {
+    .put("http://af9c5b888c5104d81bb1bd90f57e83ab-504368976.eu-west-2.elb.amazonaws.com:8081/ticket/completed/" + id, ticketValues, {
     headers: {
         'Access-Control-Allow-Origin' : '*'
         }
