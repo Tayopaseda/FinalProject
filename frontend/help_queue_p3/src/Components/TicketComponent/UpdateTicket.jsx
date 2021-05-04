de3294b4e6e6f614a78f526ce0710544df83ab21
Tayo <t.paseda@warwick.ac.uk>
2021-05-04 12:00:41 +0100

    Update UpdateTicket.jsx

--- frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.jsx
+++ frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.jsx
@@ -42,7 +42,7 @@ const updateData = (e) => {
     //   console.log(ticketValues);
       
     setTime(new Date().toLocaleString("en-GB"))
-    axios.put("http://a3129e30ad6a545b0bee2a8723b3a26e-2018857003.eu-west-2.elb.amazonaws.com:8081/ticket/update/" + id, ticketValues,  {
+    axios.put("http://af9c5b888c5104d81bb1bd90f57e83ab-504368976.eu-west-2.elb.amazonaws.com:8081/ticket/update/" + id, ticketValues,  {
       headers: {
         'Access-Control-Allow-Origin' : '*'
         }
