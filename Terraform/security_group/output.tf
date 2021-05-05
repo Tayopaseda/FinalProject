193f68491949b4a18c1a3a918ea3ce05cc7037ad
tayo-paseda <tayopaseda@gmail.com>
2021-05-05 09:36:42 +0100

    removed test db

--- Terraform/security_group/output.tf
+++ Terraform/security_group/output.tf
@@ -2,10 +2,6 @@ output "test-vm" {
   value = aws_security_group.test-vm.id
 }
 
-output "test-db" {
-  value = aws_security_group.test-db.id
-}
-
 output "prod-db" {
   value = aws_security_group.prod-db.id
 }
