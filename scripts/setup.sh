a9be64d4856955b63a5fe9b84f3d3948fcec5afa
tayo-paseda <tayopaseda@gmail.com>
2021-05-05 10:02:14 +0100

    updated jenkinsfile

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,6 +8,9 @@ kubectl apply -f nginx-config.yaml
 kubectl apply -f nginx-lb.yaml -f nginx.yaml
 sleep 3
 
+cat > ~/.bash_profile << EOF
 export backend_endpoint=$(kubectl get services nginx-lb)
+EOF
+
 cd..
 sed -i "s/{{endpoint}}/$backend_endpoint/g"  scripts/frontend-setup.sh
