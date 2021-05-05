fcebec3a716333f0e7d60bff7135d8084f1173fc
tayo-paseda <tayopaseda@gmail.com>
2021-05-05 13:16:11 +0100

    rolling update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ kubectl config set-context --current --namespace=production
 kubectl apply -f nginx-config.yaml 
 kubectl apply -f nginx-lb.yaml -f nginx.yaml -f backend.yaml -f backend-dep.yaml -f frontend.yaml -f frontend-dep.yaml 
 
-kubectl rollout restart deployment backend
+kubectl set image deployments/backend backend=tayop/backend:latest
 kubectl rollout restart deployment help-queue
 
 sleep 3
