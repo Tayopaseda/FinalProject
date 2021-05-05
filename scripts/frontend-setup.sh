aae4bff293429b7ccf5684a80639598c6ebbedf2
tayo-paseda <tayopaseda@gmail.com>
2021-05-05 12:32:25 +0100

    switch file type

--- scripts/frontend-setup.sh
+++ scripts/frontend-setup.sh
@@ -3,10 +3,22 @@
 export backend={{endpoint}}
 export database={{cluster-address}}
 
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.jsx
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.jsx
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.jsx
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/ViewTickets.jsx
+mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.jsx /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.txt
+mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt
+mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.txt
+mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.txt
+mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/ViewTicket.txt
+
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.txt
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.txt
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.txt
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/ViewTickets.txt
 
 sed -i "s/{{database-endpoint}}/$database/g" /home/ubuntu/shiny-funicular/CNE-Final-Project/src/main/resources/application-dev.properties
+
+mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.txt /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.jsx
+mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx
+mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.jsx
+mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.jsx
+mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/ViewTicket.jsx
