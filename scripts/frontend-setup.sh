64cad4d67863986df8f28dd065909deea3634e24
tayo-paseda <tayopaseda@gmail.com>
2021-05-05 10:22:36 +0100

    fixed typo

--- scripts/frontend-setup.sh
+++ scripts/frontend-setup.sh
@@ -9,4 +9,4 @@ sed -i "s/localhost:8080/$backend/g" /home/ubuntu/shiny-funicular/frontend/help_
 sed -i "s/localhost:8080/$backend/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.jsx
 sed -i "s/localhost:8080/$backend/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/ViewTickets.jsx
 
-sed -i "s/{{database-enpoint}}/$database/g" /home/ubuntu/shiny-funicular/CNE-Final-Project/src/main/resources/application-dev.properties
+sed -i "s/{{database-endpoint}}/$database/g" /home/ubuntu/shiny-funicular/CNE-Final-Project/src/main/resources/application-dev.properties
