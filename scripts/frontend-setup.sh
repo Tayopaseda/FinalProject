f1ef79f8a6660ff007ee76f231a17e421aeacf80
tayo-paseda <tayopaseda@gmail.com>
2021-05-05 09:53:42 +0100

    added scripts

---
+++ scripts/frontend-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export backend="{{endpoint}}:8081"
+
+sed -i "s/localhost:8080/$backend/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.jsx
+sed -i "s/localhost:8080/$backend/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx
+sed -i "s/localhost:8080/$backend/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.jsx
+sed -i "s/localhost:8080/$backend/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.jsx
+sed -i "s/localhost:8080/$backend/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/ViewTickets.jsx
+
---
+++ scripts/setup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+cd ./kubernetes
+
+kubectl apply -f namespace.yaml
+kubectl config set-context --current --namespace=production
+kubectl apply -f nginx-config.yaml 
+kubectl apply -f nginx-lb.yaml -f nginx.yaml
+sleep 3
+
+export backend_endpoint=$(kubectl get services nginx-lb)
+cd..
+sed -i "s/{{endpoint}}/$backend_endpoint/g"  scripts/frontend-setup.sh
