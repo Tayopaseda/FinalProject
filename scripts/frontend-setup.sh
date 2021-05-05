43a64aacb6bff75db203eb42f3114c984e6e1e6b
tayo-paseda <tayopaseda@gmail.com>
2021-05-05 12:47:01 +0100

    updated sed

--- scripts/frontend-setup.sh
+++ scripts/frontend-setup.sh
@@ -3,22 +3,13 @@
 export backend={{endpoint}}
 export database={{cluster-address}}
 
-mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.jsx /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.txt
-mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt
-mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.txt
-mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.txt
-mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/ViewTicket.txt
 
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.txt
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.txt
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.txt
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/ViewTickets.txt
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/HomePage/Main.jsx
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.jsx
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.jsx
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/ViewTickets.jsx
 
 sed -i "s/{{database-endpoint}}/$database/g" /home/ubuntu/shiny-funicular/CNE-Final-Project/src/main/resources/application-dev.properties
 
-mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.txt /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/Homepage/Main.jsx
-mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx
-mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.jsx
-mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.jsx
-mv /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.txt /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/ViewTicket.jsx
+
