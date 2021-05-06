6215406e27dd24e40815c178e47ca1b160fd8670
sashumi <55672042+sashumi@users.noreply.github.com>
2021-05-06 14:51:10 +0100

    Create App.js

---
+++ frontend/help_queue_p3/src/App.js
@@ -0,0 +1,14 @@
+import Main from "./Components/HomePage/Main";
+import Banner from "./Components/HomePage/Banner";
+function App() {
+  return (
+    <body id="testBody">
+
+        <Banner/>
+        <Main/>
+
+    </body>
+  );
+}
+
+export default App;
