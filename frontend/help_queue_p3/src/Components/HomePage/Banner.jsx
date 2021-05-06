4e7676ad4fbbc43ad1322d6502cfe9fdb4367745
sashumi <55672042+sashumi@users.noreply.github.com>
2021-05-06 14:57:39 +0100

    Create Banner.jsx

---
+++ frontend/help_queue_p3/src/Components/HomePage/Banner.jsx
@@ -0,0 +1,16 @@
+import logo from '../../css/images/Banner.png'
+
+
+const Banner = () => {
+
+    return(
+            <div>
+                <img src={logo} alt ="Logo" id="mainBanner"/>;
+            </div>
+            )  
+
+
+}
+
+export default Banner;
+© 2021 GitHub, Inc.
