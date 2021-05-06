098fbf350e506f5817eb40414aacd8c9fe8ad155
sashumi <55672042+sashumi@users.noreply.github.com>
2021-05-06 14:53:17 +0100

    Create Banner.jsx

---
+++ frontend/help_queue_p3/src/Components/TicketComponent/HomePage/Banner.jsx
@@ -0,0 +1,15 @@
+import logo from '../../css/images/Banner.png'
+
+
+const Banner = () => {
+
+    return(
+            <div>
+                <img src={logo} alt ="Logo" id="mainBanner"/>;
+            </div>
+            )  
+
+
+}
+
+export default Banner;
