bc745b7b021a62ce095f578f61b9cb27b28ac071
Tayo <t.paseda@warwick.ac.uk>
2021-05-10 14:49:22 +0100

    Update frontend-setup.sh

--- scripts/frontend-setup.sh
+++ scripts/frontend-setup.sh
@@ -4,12 +4,12 @@ export backend={{endpoint}}
 export database={{cluster-address}}
 
 
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/HomePage/Main.jsx
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.jsx
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.jsx
-sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/shiny-funicular/frontend/help_queue_p3/src/Components/TicketComponent/ViewTickets.jsx
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/FinalProject/frontend/help_queue_p3/src/Components/HomePage/Main.jsx
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/FinalProject/frontend/help_queue_p3/src/Components/TicketComponent/CreateTicket.jsx
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/FinalProject/frontend/help_queue_p3/src/Components/TicketComponent/DeleteTicket.jsx
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/FinalProject/frontend/help_queue_p3/src/Components/TicketComponent/UpdateTicket.jsx
+sed -i "s/localhost:8080/$backend:8081/g" /home/ubuntu/FinalProject/frontend/help_queue_p3/src/Components/TicketComponent/ViewTickets.jsx
 
-sed -i "s/{{database-endpoint}}/$database/g" /home/ubuntu/shiny-funicular/CNE-Final-Project/src/main/resources/application-dev.properties
+sed -i "s/{{database-endpoint}}/$database/g" //home/ubuntu/FinalProject/CNE-Final-Project/src/main/resources/application-dev.properties
 
 
